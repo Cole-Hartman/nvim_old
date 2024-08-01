747053b93b73c9f1efc1e059b201c89c248379e3
Cole <colehartman00@gmail.com>
2024-07-31 20:12:12 -0700

    'update'

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -142,4 +142,130 @@ return {
       -- add any custom options here
     },
   },
+
+  -- Toggle Term Plugin
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    config = function()
+      require('toggleterm').setup {
+        -- customization
+        shade_terminals = false,
+        open_mapping = [[<c-\>]],
+        autochdir = false,
+      }
+
+      -- -- Function to open terminal in the current file's directory
+      -- local function open_project_terminal()
+      --   -- Open the first terminal with default directory
+      --   vim.cmd 'ToggleTerm'
+      --   vim.cmd 'wincmd p' -- Go back to the previous window
+      --
+      --   -- Change directory to the current file's directory
+      --   vim.cmd 'lcd %:h'
+      --
+      --   -- Open the second terminal in the new directory
+      --   vim.cmd 'ToggleTerm'
+      -- end
+      --
+      -- -- Create a command to call the function
+      -- vim.api.nvim_create_user_command('ProjectTerminal', open_project_terminal, {})
+
+      -- keymaps
+      function _G.set_terminal_keymaps()
+        local opts = { buffer = 0 }
+        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+        vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+        vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+      end
+    end,
+  },
+
+  -- Edgy Window Plugin
+  -- {
+  --   'folke/edgy.nvim',
+  --   event = 'VeryLazy',
+  --   init = function()
+  --     vim.opt.laststatus = 3
+  --     vim.opt.splitkeep = 'screen'
+  --   end,
+  --   opts = {
+  --     bottom = {
+  --       -- toggleterm / lazyterm at the bottom with a height of 40% of the screen
+  --       {
+  --         ft = 'toggleterm',
+  --         size = { height = 0.4 },
+  --         -- exclude floating windows
+  --         filter = function(buf, win)
+  --           return vim.api.nvim_win_get_config(win).relative == ''
+  --         end,
+  --       },
+  --       {
+  --         ft = 'lazyterm',
+  --         title = 'LazyTerm',
+  --         size = { height = 0.4 },
+  --         filter = function(buf)
+  --           return not vim.b[buf].lazyterm_cmd
+  --         end,
+  --       },
+  --       'Trouble',
+  --       { ft = 'qf', title = 'QuickFix' },
+  --       {
+  --         ft = 'help',
+  --         size = { height = 20 },
+  --         -- only show help buffers
+  --         filter = function(buf)
+  --           return vim.bo[buf].buftype == 'help'
+  --         end,
+  --       },
+  --       { ft = 'spectre_panel', size = { height = 0.4 } },
+  --     },
+  --     left = {
+  --       -- Neo-tree filesystem always takes half the screen height
+  --       {
+  --         title = 'Neo-Tree',
+  --         ft = 'neo-tree',
+  --         filter = function(buf)
+  --           return vim.b[buf].neo_tree_source == 'filesystem'
+  --         end,
+  --         size = { height = 0.5 },
+  --       },
+  --       {
+  --         title = 'Neo-Tree Git',
+  --         ft = 'neo-tree',
+  --         filter = function(buf)
+  --           return vim.b[buf].neo_tree_source == 'git_status'
+  --         end,
+  --         pinned = true,
+  --         collapsed = true, -- show window as closed/collapsed on start
+  --         open = 'Neotree position=right git_status',
+  --       },
+  --       {
+  --         title = 'Neo-Tree Buffers',
+  --         ft = 'neo-tree',
+  --         filter = function(buf)
+  --           return vim.b[buf].neo_tree_source == 'buffers'
+  --         end,
+  --         pinned = true,
+  --         collapsed = true, -- show window as closed/collapsed on start
+  --         open = 'Neotree position=top buffers',
+  --       },
+  --       {
+  --         title = function()
+  --           local buf_name = vim.api.nvim_buf_get_name(0) or '[No Name]'
+  --           return vim.fn.fnamemodify(buf_name, ':t')
+  --         end,
+  --         ft = 'Outline',
+  --         pinned = true,
+  --         open = 'SymbolsOutlineOpen',
+  --       },
+  --       -- any other neo-tree windows
+  --       'neo-tree',
+  --     },
+  --   },
+  -- },
 }
