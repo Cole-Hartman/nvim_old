96d3218f97c525fae61ef372e0ec24b61a784182
Cole <colehartman00@gmail.com>
2024-08-22 12:27:38 -0700

    plugin - markdown preview

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -155,22 +155,6 @@ return {
         autochdir = false,
       }
 
-      -- -- Function to open terminal in the current file's directory
-      -- local function open_project_terminal()
-      --   -- Open the first terminal with default directory
-      --   vim.cmd 'ToggleTerm'
-      --   vim.cmd 'wincmd p' -- Go back to the previous window
-      --
-      --   -- Change directory to the current file's directory
-      --   vim.cmd 'lcd %:h'
-      --
-      --   -- Open the second terminal in the new directory
-      --   vim.cmd 'ToggleTerm'
-      -- end
-      --
-      -- -- Create a command to call the function
-      -- vim.api.nvim_create_user_command('ProjectTerminal', open_project_terminal, {})
-
       -- keymaps
       function _G.set_terminal_keymaps()
         local opts = { buffer = 0 }
@@ -185,87 +169,14 @@ return {
     end,
   },
 
-  -- Edgy Window Plugin
-  -- {
-  --   'folke/edgy.nvim',
-  --   event = 'VeryLazy',
-  --   init = function()
-  --     vim.opt.laststatus = 3
-  --     vim.opt.splitkeep = 'screen'
-  --   end,
-  --   opts = {
-  --     bottom = {
-  --       -- toggleterm / lazyterm at the bottom with a height of 40% of the screen
-  --       {
-  --         ft = 'toggleterm',
-  --         size = { height = 0.4 },
-  --         -- exclude floating windows
-  --         filter = function(buf, win)
-  --           return vim.api.nvim_win_get_config(win).relative == ''
-  --         end,
-  --       },
-  --       {
-  --         ft = 'lazyterm',
-  --         title = 'LazyTerm',
-  --         size = { height = 0.4 },
-  --         filter = function(buf)
-  --           return not vim.b[buf].lazyterm_cmd
-  --         end,
-  --       },
-  --       'Trouble',
-  --       { ft = 'qf', title = 'QuickFix' },
-  --       {
-  --         ft = 'help',
-  --         size = { height = 20 },
-  --         -- only show help buffers
-  --         filter = function(buf)
-  --           return vim.bo[buf].buftype == 'help'
-  --         end,
-  --       },
-  --       { ft = 'spectre_panel', size = { height = 0.4 } },
-  --     },
-  --     left = {
-  --       -- Neo-tree filesystem always takes half the screen height
-  --       {
-  --         title = 'Neo-Tree',
-  --         ft = 'neo-tree',
-  --         filter = function(buf)
-  --           return vim.b[buf].neo_tree_source == 'filesystem'
-  --         end,
-  --         size = { height = 0.5 },
-  --       },
-  --       {
-  --         title = 'Neo-Tree Git',
-  --         ft = 'neo-tree',
-  --         filter = function(buf)
-  --           return vim.b[buf].neo_tree_source == 'git_status'
-  --         end,
-  --         pinned = true,
-  --         collapsed = true, -- show window as closed/collapsed on start
-  --         open = 'Neotree position=right git_status',
-  --       },
-  --       {
-  --         title = 'Neo-Tree Buffers',
-  --         ft = 'neo-tree',
-  --         filter = function(buf)
-  --           return vim.b[buf].neo_tree_source == 'buffers'
-  --         end,
-  --         pinned = true,
-  --         collapsed = true, -- show window as closed/collapsed on start
-  --         open = 'Neotree position=top buffers',
-  --       },
-  --       {
-  --         title = function()
-  --           local buf_name = vim.api.nvim_buf_get_name(0) or '[No Name]'
-  --           return vim.fn.fnamemodify(buf_name, ':t')
-  --         end,
-  --         ft = 'Outline',
-  --         pinned = true,
-  --         open = 'SymbolsOutlineOpen',
-  --       },
-  --       -- any other neo-tree windows
-  --       'neo-tree',
-  --     },
-  --   },
-  -- },
+  -- Markdown Preview
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && npm install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  },
 }
