60c421ff990ae7345bb5b2ae8ed72e83241e63eb
cole-hartman <colehartman00@gmail.com>
2025-01-24 22:45:13 -0800

    u

--- init.lua
+++ init.lua
@@ -461,6 +461,8 @@ require('lazy').setup({
         html = {},
         cssls = {},
         tailwindcss = {},
+        hls = {},
+
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
